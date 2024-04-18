ba0afd1568e3004e0a0470eb338d9ee35a1236de
marcushattula <marcus.hattula@gmail.com>
2024-04-18 19:41:55 +0300

    Initial commit for unittests.py

---
+++ unittests.py
@@ -0,0 +1,74 @@
+import unittest
+
+class TestGlobals(unittest.TestCase):
+    """
+    Testclass includes tests for functions and methods defined in globals.py
+    """
+
+    def testRemoveAccents(self):
+        # TODO: Implement
+        self.assertTrue(False)
+    
+    def testFindObjects(self):
+        # TODO: Implement
+        self.assertTrue(False)
+    
+    def testFindSingleObject(self):
+        # TODO: Implement
+        self.assertTrue(False)
+
+
+class TestArchiveReader(unittest.TestCase):
+    """
+    Testclass includes tests for testing ArchiveReader class
+    """
+
+    def testReader(self):
+        self.assertTrue(False)
+
+
+class TestMyDataClasses(unittest.TestCase):
+    """
+    Testclass includes tests for MyDataClass objects and derivatives
+    """
+
+    def testMyDataClass(self):
+        # TODO: Implement
+        self.assertTrue(False) 
+    
+    def testConstructorClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+    
+    def testDriverClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+    
+    def testRaceClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+
+    def testSeasonClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+
+class TestQuestions(unittest.TestCase):
+    """
+    Testclass includes tests for Question class
+    """
+
+    def testQuestionClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+
+class TestQuizClass(unittest.TestCase):
+    """
+    Testclass includes tests for Quiz class
+    """
+
+    def testQuizClass(self):
+        # TODO: Implement
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
